dad981716f4371bcc2f62a043754ce83ba202b18
homa <homa@homa7.com>
2021-10-29 21:06:25 -0400

    more init...

--- apps/black-tail-app/src/App.tsx
+++ apps/black-tail-app/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Button } from 'antd';
 
 function App() {
-  return (<p>Hello World</p>);
+  return (<p>Hello World <Button type="primary">Button</Button></p>);
 }
 
 export default App;
--- apps/black-tail-app/src/index.tsx
+++ apps/black-tail-app/src/index.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
